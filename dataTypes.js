329ec070b0664dd796053e74ed51e16d344d174a
Vishwas Rawat <123429318+Vishwas-Rawat@users.noreply.github.com>
2024-10-30 22:39:43 -0700

    Add files via upload

---
+++ dataTypes.js
@@ -0,0 +1,15 @@
+// Number
+let a = 50
+let b = 12.54
+console.log(typeof(b))
+// console.log(typeof(a))
+
+// String
+let c = 'kanha'
+// console.log(typeof(c))
+// console.log(c)
+
+// Boolean
+let d = false
+console.log(typeof(d))
+console.log(d)
