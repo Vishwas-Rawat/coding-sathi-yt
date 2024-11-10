f066ea073eeaa7989ff3a40fc1d24ffd6b58db1b
Vishwas Rawat <123429318+Vishwas-Rawat@users.noreply.github.com>
2024-11-10 00:58:05 -0800

    Add files via upload

---
+++ namingConventions.js
@@ -0,0 +1,21 @@
+// Variable should have a vaild name
+let age = 10;
+
+// Can start with an underscore(_)
+let _number1 = 12;
+
+// Cannot start with a number (Throws an error)
+// let 2number = 45;
+
+
+// Case Senstive
+let player = "xyz";
+let PLAYER = "abc";
+console.log(player)
+console.log(PLAYER)
+
+
+// CamelCase
+let firstPlayerServer1;
+let jsDeveloper = "Kanha";
+console.log(jsDeveloper);
